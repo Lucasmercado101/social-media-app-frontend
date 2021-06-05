89efab8cd08060052fced27cad0b997f189d1daa
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 14:03:07 -0300

    added home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Redirect, Switch } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Home from "./Pages/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/home" component={Home} />
         <Redirect to="login" />
       </Switch>
     </div>
