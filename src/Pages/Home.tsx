60273a594ab3b861e80c9b2af70a87b62a0e83f3
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 16:48:02 -0300

    nav redirects you respectively

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,9 +5,11 @@ import HomeIcon from "@material-ui/icons/Home";
 import Feed from "../Components/Feed";
 import PeopleIcon from "@material-ui/icons/People";
 import PersonIcon from "@material-ui/icons/Person";
+import { useHistory } from "react-router-dom";
 
 function Home() {
   const [navigationValue, setNavigationValue] = useState(0);
+  const history = useHistory();
 
   return (
     <div>
@@ -24,9 +26,29 @@ function Home() {
         showLabels
         style={{ position: "sticky", bottom: 0 }}
       >
-        <BottomNavigationAction label="Feed" icon={<HomeIcon />} />
-        <BottomNavigationAction label="Explore" icon={<PeopleIcon />} />
-        <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+        <BottomNavigationAction
+          onClick={() => {
+            if (history.location.pathname !== "/home") history.push("/home");
+          }}
+          label="Feed"
+          icon={<HomeIcon />}
+        />
+        <BottomNavigationAction
+          onClick={() => {
+            if (history.location.pathname !== "/explore")
+              history.push("/explore");
+          }}
+          label="Explore"
+          icon={<PeopleIcon />}
+        />
+        <BottomNavigationAction
+          onClick={() => {
+            if (history.location.pathname !== "/profile")
+              history.push("/profile");
+          }}
+          label="Profile"
+          icon={<PersonIcon />}
+        />
       </BottomNavigation>
     </div>
   );
