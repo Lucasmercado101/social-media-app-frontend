c5be4f309ac70397d6c0d349a6cf5fa0be1a2c0d
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 16:40:22 -0300

    added base bottom navigation

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import { Route, Redirect, Switch } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
 import Feed from "../Components/Feed";
+import PeopleIcon from "@material-ui/icons/People";
+import PersonIcon from "@material-ui/icons/Person";
 
 function Home() {
+  const [navigationValue, setNavigationValue] = useState(0);
+
   return (
     <div>
       <Switch>
@@ -9,6 +16,18 @@ function Home() {
         {/* <Route path={`/home/explore`} exact component={Feed} /> */}
         <Redirect to="/home" />
       </Switch>
+      <BottomNavigation
+        value={navigationValue}
+        onChange={(event, newValue) => {
+          setNavigationValue(newValue);
+        }}
+        showLabels
+        style={{ position: "sticky", bottom: 0 }}
+      >
+        <BottomNavigationAction label="Feed" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Explore" icon={<PeopleIcon />} />
+        <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+      </BottomNavigation>
     </div>
   );
 }
