97c9dffac7c3a63fac65aa5b8acb13a7d15515c0
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 19:07:31 -0300

    added queryClient provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import App from "./App";
 import ThemeProvider from "./ThemeProvider";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
