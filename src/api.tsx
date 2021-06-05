7e8869701e0bad51f55565eb5716e16f15f47105
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 12:50:51 -0300

    added register

--- src/api.tsx
+++ src/api.tsx
@@ -2,3 +2,11 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.withCredentials = true;
+
+interface registerParams {
+  username: string;
+  password: string;
+}
+export function register(data: registerParams) {
+  return axios.post("/auth/register", data);
+}
