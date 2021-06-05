9e9eff8ef7b732a7d8ce9da36c5b95ec76763f35
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 18:29:04 -0300

    new post machine is complete

--- src/Components/NewPost/newPostMachine.ts
+++ src/Components/NewPost/newPostMachine.ts
@@ -1,14 +1,42 @@
-import { Machine } from "xstate";
+import { Machine, assign } from "xstate";
 
-export const newPostMachine = Machine({
-  id: "new-post",
-  initial: "idle",
-  states: {
-    idle: {
-      on: {
-        start_creating_new_post: "creating_new_post"
-      }
+interface context {
+  content: string;
+}
+
+export const newPostMachine = Machine<context>(
+  {
+    id: "new-post",
+    initial: "idle",
+    context: {
+      content: ""
     },
-    creating_new_post: {}
+    states: {
+      idle: {
+        entry: "emptyContent",
+        on: {
+          start_creating_new_post: "creating_new_post"
+        }
+      },
+      creating_new_post: {
+        on: {
+          typed: {
+            actions: "typedContent"
+          },
+          discard: "idle",
+          done: "done"
+        }
+      },
+      done: {
+        entry: "onDone",
+        always: "idle"
+      }
+    }
+  },
+  {
+    actions: {
+      typedContent: assign({ content: (_, e) => e.content }),
+      emptyContent: assign({ content: "" })
+    }
   }
-});
+);
