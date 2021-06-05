74e22903afcf17997dd89ef730cb9f349ef5d174
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 11:15:21 -0300

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ThemeProvider from "./ThemeProvider";
-// import {brow} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
