a4babf5dda207527d8ccaf209f9e79c47e605e35
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 13:48:05 -0300

    register uses registerMachine

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -4,11 +4,15 @@ import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Collapse from "@material-ui/core/Collapse";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
 import { useForm } from "react-hook-form";
-import { Link as RouterLink } from "react-router-dom";
-import { register as registerApi } from "../api";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import { registerMachine } from "./registerMachine";
+import { useMachine } from "@xstate/react";
+import { Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,28 +44,45 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
+  },
+  errorAlert: { width: "100%", marginTop: 15 },
+  buttonProgress: {
+    color: theme.palette.secondary.main,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12
   }
 }));
 
 export default function Login() {
   const classes = useStyles();
+  const history = useHistory();
   const { register, handleSubmit } = useForm();
-
-  const onSubmit = (values: { username: string; password: string }) => {
-    registerApi(values);
-  };
+  const [state, send] = useMachine(registerMachine, {
+    actions: {
+      onDone: () => history.replace("/login")
+    }
+  });
 
   return (
     <Grid container component="main" className={classes.root}>
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
+
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar} />
           <Typography component="h1" variant="h5">
             Register
           </Typography>
+          <Collapse in={state.matches("error")} className={classes.errorAlert}>
+            <Alert severity="error">{state.context.error}</Alert>
+          </Collapse>
           <form
-            onSubmit={handleSubmit(onSubmit)}
+            onSubmit={handleSubmit((values) =>
+              send({ type: "register", data: values })
+            )}
             className={classes.form}
             noValidate
           >
@@ -92,8 +113,15 @@ export default function Login() {
               variant="contained"
               color="primary"
               className={classes.submit}
+              disabled={state.matches("registering")}
             >
               Register
+              {state.matches("registering") && (
+                <CircularProgress
+                  size={24}
+                  className={classes.buttonProgress}
+                />
+              )}
             </Button>
             <Grid container>
               <Grid item>
