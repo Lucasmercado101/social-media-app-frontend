31884f2a27b9a8d8bcd1fd365592bc1da62ba89f
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-05 12:51:14 -0300

    invalid routes redirects to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 
 function App() {
   return (
     <div>
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Redirect to="login" />
+      </Switch>
     </div>
   );
 }
