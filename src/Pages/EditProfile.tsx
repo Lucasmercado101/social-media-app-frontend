c19de902ebae0db7f2f839de6326c1d4c02b215d
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-06 19:25:32 -0300

    added state machine and input depends on state

--- src/Pages/EditProfile.tsx
+++ src/Pages/EditProfile.tsx
@@ -7,21 +7,32 @@ import {
   Box,
   Avatar,
   TextField,
-  Button
+  Button,
+  InputAdornment
 } from "@material-ui/core";
-import { useForm } from "react-hook-form";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { useHistory } from "react-router-dom";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import CloseIcon from "@material-ui/icons/Close";
 import EditIcon from "@material-ui/icons/Edit";
+import { editProfileMachine } from "./editProfileMachine";
+import { useMachine } from "@xstate/react";
+import { getMyUserData } from "../api";
+import { useQuery } from "react-query";
 
 function EditProfile() {
-  const { register } = useForm();
+  const { data } = useQuery("my user data", getMyUserData);
   const scrolled = useScrollTrigger({
     disableHysteresis: true,
     threshold: 10
   });
   const history = useHistory();
+  const [state, send] = useMachine(editProfileMachine);
+  const { username } = state.context;
+
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
@@ -44,7 +55,7 @@ function EditProfile() {
           <Typography variant="h6">Edit profile</Typography>
         </Toolbar>
       </AppBar>
-      <Box m={2}>
+      <Box component="form" onSubmit={handleSubmit} m={2}>
         <div style={{ maxHeight: 200 }}>
           <Box clone mx="auto">
             <div
@@ -76,35 +87,60 @@ function EditProfile() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <div>
-            <Box clone color="text.secondary">
-              <Typography>Username:</Typography>
+          {state.matches({ username: "default" }) && (
+            <>
+              <div>
+                <Box clone color="text.secondary">
+                  <Typography>Username:</Typography>
+                </Box>
+                <Typography>Johnathan Doughie</Typography>
+              </div>
+              <IconButton
+                onClick={() => send({ type: "start_editing_username" })}
+                size="small"
+              >
+                <EditIcon />
+              </IconButton>
+            </>
+          )}
+          {state.matches({ username: "editing" }) && (
+            <Box clone width={1}>
+              <TextField
+                name="username"
+                variant="outlined"
+                placeholder="Johnathan Doughie"
+                label="Username"
+                autoFocus
+                value={username}
+                onChange={(e) =>
+                  send({ type: "edited_username", value: e.target.value })
+                }
+                required={true}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment
+                      onClick={() => send({ type: "stop_editing_username" })}
+                      position="end"
+                    >
+                      <CloseIcon />
+                    </InputAdornment>
+                  )
+                }}
+              />
             </Box>
-            <Typography>Johnathan Doughie</Typography>
-          </div>
-          <IconButton size="small">
-            <EditIcon />
-          </IconButton>
+          )}
         </Box>
         <Box mt={4} display="flex" justifyContent="space-between">
           <Box clone color="error.main" borderColor="error.main">
-            <Button
-              onClick={() =>
-                history.length > 1
-                  ? history.goBack()
-                  : history.push("/home/profile")
-              }
-              variant="outlined"
-            >
+            <Button variant="outlined" type="submit">
               Cancel
             </Button>
           </Box>
-          <Button variant="contained" color="primary">
+          <Button type="submit" variant="contained" color="primary">
             Confirm
           </Button>
         </Box>
       </Box>
-      {/* <TextField variant="outlined" /> */}
     </div>
   );
 }
