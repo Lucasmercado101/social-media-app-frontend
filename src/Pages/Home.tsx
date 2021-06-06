9b9744b1455284b5fcdb04e1ea4a197fad294535
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-06 16:45:41 -0300

    added editProfile and bottom navigation shows up on feed, explore and profile only

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,6 +5,7 @@ import HomeIcon from "@material-ui/icons/Home";
 import Feed from "../Components/Feed";
 import Explore from "../Components/Explore";
 import Profile from "./Profile";
+import EditProfile from "./EditProfile";
 import PeopleIcon from "@material-ui/icons/People";
 import PersonIcon from "@material-ui/icons/Person";
 import { useHistory } from "react-router-dom";
@@ -41,42 +42,44 @@ function Home() {
           <Route path={"/home"} exact component={Feed} />
           <Route path={"/home/explore"} exact component={Explore} />
           <Route path={"/home/profile"} exact component={Profile} />
+          <Route path={"/home/profile/edit"} exact component={EditProfile} />
           <Redirect to="/home" />
         </Switch>
       </div>
-
-      <BottomNavigation
-        value={navigationValue}
-        onChange={(event, newValue) => {
-          setNavigationValue(newValue);
-        }}
-        showLabels
-        style={{ position: "sticky", bottom: 0 }}
-      >
-        <BottomNavigationAction
-          onClick={() => {
-            if (history.location.pathname !== "/home") history.push("/home");
+      <Route exact path="(/home/explore|/home/profile|/home)">
+        <BottomNavigation
+          value={navigationValue}
+          onChange={(event, newValue) => {
+            setNavigationValue(newValue);
           }}
-          label="Feed"
-          icon={<HomeIcon />}
-        />
-        <BottomNavigationAction
-          onClick={() => {
-            if (history.location.pathname !== "/home/explore")
-              history.push("/home/explore");
-          }}
-          label="Explore"
-          icon={<PeopleIcon />}
-        />
-        <BottomNavigationAction
-          onClick={() => {
-            if (history.location.pathname !== "/home/profile")
-              history.push("/home/profile");
-          }}
-          label="Profile"
-          icon={<PersonIcon />}
-        />
-      </BottomNavigation>
+          showLabels
+          style={{ position: "sticky", bottom: 0 }}
+        >
+          <BottomNavigationAction
+            onClick={() => {
+              if (history.location.pathname !== "/home") history.push("/home");
+            }}
+            label="Feed"
+            icon={<HomeIcon />}
+          />
+          <BottomNavigationAction
+            onClick={() => {
+              if (history.location.pathname !== "/home/explore")
+                history.push("/home/explore");
+            }}
+            label="Explore"
+            icon={<PeopleIcon />}
+          />
+          <BottomNavigationAction
+            onClick={() => {
+              if (history.location.pathname !== "/home/profile")
+                history.push("/home/profile");
+            }}
+            label="Profile"
+            icon={<PersonIcon />}
+          />
+        </BottomNavigation>
+      </Route>
     </div>
   );
 }
