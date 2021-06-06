11b06b245a6ee836f631cfb27493174c78f47c65
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-06 11:43:28 -0300

    created explore

---
+++ src/Components/Explore.tsx
@@ -0,0 +1,80 @@
+import Post from "./Post";
+import { makeStyles, CircularProgress, Box } from "@material-ui/core";
+import { subDays } from "date-fns";
+import NewPost from "./NewPost/NewPost";
+import { useInfiniteQuery } from "react-query";
+import { getExplore } from "../api";
+
+const useStyles = makeStyles((theme) => ({
+  exploreColumnList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 15,
+    paddingBottom: 15,
+    padding: 0,
+    margin: 0,
+    listStyleType: "none"
+  }
+}));
+
+function Explore() {
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status
+  } = useInfiniteQuery(
+    "explore",
+    ({ pageParam = 1 }) => getExplore({ limit: 15, page: pageParam }),
+    {
+      getNextPageParam: (lastPage) => lastPage.next?.page
+    }
+  );
+  const classes = useStyles();
+  console.log(data);
+
+  return (
+    <div>
+      <div style={{ marginBottom: 15 }}>
+        <NewPost />
+      </div>
+      {data &&
+        data.pages.map(({ results }) => (
+          <ul className={classes.exploreColumnList}>
+            {results.map(({ authorId, content, createdAt, id }) => (
+              <li key={id}>
+                <Post
+                  authorId={authorId}
+                  body={content}
+                  postedAt={new Date(createdAt)}
+                />
+              </li>
+            ))}
+          </ul>
+        ))}
+
+      {(isFetching || isFetchingNextPage) && (
+        <Box display="flex" justifyContent="center" mt={3}>
+          <CircularProgress size={60} />
+        </Box>
+      )}
+      {/* <div className={classes.feedColumn}>
+        {dummyData.map(({ profilePic, id, username, body, contentImage }) => (
+          <Post
+            username={username}
+            date={subDays(new Date(), 4 * id)}
+            key={id}
+            body={body}
+            image={contentImage}
+            profilePictureUrl={profilePic}
+          />
+        ))}
+      </div> */}
+    </div>
+  );
+}
+
+export default Explore;
