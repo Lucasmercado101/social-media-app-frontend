f3e8fa96398bbe5a94c13e646636f2e18f25cb5f
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-08 09:30:54 -0300

    added appbar

--- src/Pages/Friends.tsx
+++ src/Pages/Friends.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+  Box
+} from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { useHistory } from "react-router-dom";
 
 function Friends() {
-  return <div></div>;
+  const history = useHistory();
+  const scrolled = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 10
+  });
+
+  return (
+    <div>
+      <AppBar position="sticky" elevation={scrolled ? 4 : 0}>
+        <Toolbar>
+          <Box clone mr={1}>
+            <IconButton
+              onClick={() =>
+                history.length > 1
+                  ? history.goBack()
+                  : history.push("/home/profile")
+              }
+              edge="start"
+              color="inherit"
+            >
+              <ArrowBackIcon />
+            </IconButton>
+          </Box>
+          <Typography variant="h6">Friends</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Friends;
